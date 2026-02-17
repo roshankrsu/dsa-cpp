af0c353be156fef1592b0d6a7b703df34b624d17
Roshan <roshankrsu@gmail.com>
2026-02-17 19:07:13 +0530

    added one more pattern

--- Pattern/pattern1.cpp
+++ Pattern/pattern1.cpp
@@ -59,6 +59,22 @@ void print9(int n){
    }  
 }
 
+void print10(int n){
+    int start = 1;
+    for(int i = 0; i < n; i++){
+        if(i % 2 ==0){
+            start = 1;
+        } else {
+            start = 0;
+        }
+         for(int j = 0; j<=i; j++){
+            cout<< start <<" ";
+            start = 1 - start;
+         }
+         cout<<endl;
+    }
+}
+
 int main(){
    print9(5);
    return 0;
